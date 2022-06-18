21c02837480d0500a18d08a6a11329acd9a43abc
Simonas-A <simonas.albrechtas@gmail.com>
2022-06-18 23:59:11 +0300

    Initial commit

---
+++ codex/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace codex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(304, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(171, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(910, 143);
+            this.textBox2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 356);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(445, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 145);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ codex/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace codex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string responseString;
+
+        string result;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Rectangle resolution = Screen.PrimaryScreen.Bounds;
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Left = 0;
+            this.Top = 0;
+            this.Width = resolution.Width;
+            this.Height = resolution.Height;
+
+            textBox2.Left = resolution.Width / 2 - textBox2.Width / 2;
+            textBox2.Top = resolution.Height / 4 - textBox2.Height / 2;
+
+            label1.Parent = pictureBox1;
+            label1.BackColor = Color.FromArgb(100, 0, 0, 0);
+            SetText("Personal code:");
+            
+
+            button1.Left = resolution.Width / 2 - button1.Width / 2;
+            button1.Top = resolution.Height / 2 + button1.Height / 2 + textBox2.Height;
+
+            pictureBox1.Top = 0;
+            pictureBox1.Height = resolution.Height;
+            pictureBox1.Width = resolution.Width;
+
+            pictureBox1.Left = (resolution.Width - pictureBox1.Width) / 2;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+
+            pictureBox1.Image = codex.Properties.Resources.safeidle;
+        }
+
+        private void SetText(string v)
+        {
+            Rectangle resolution = Screen.PrimaryScreen.Bounds;
+            label1.Text = v;
+            label1.Left = resolution.Width / 2 - label1.Width / 2;
+            label1.Top = resolution.Height / 4 - label1.Height / 2 - textBox2.Height;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var task = SendRequestAsync();
+            await task;
+
+            textBox2.Visible = false;
+            button1.Visible = false;
+
+            var data = (JObject)JsonConvert.DeserializeObject(responseString);
+
+            if (data.SelectToken("sessionId") == null)
+            {
+                label1.Text = "User not found";
+                return;
+            }
+
+            var address = data.SelectToken("sessionId").Value<string>();
+            var code = data.SelectToken("verificationCode").Value<string>();
+
+            SetText("Verification code: " + code + "\nCheck your phone");
+
+
+
+            var resultTask = WaitResponseAsync(address);
+            await resultTask;
+
+            var data1 = (JObject)JsonConvert.DeserializeObject(result);
+            var address1 = data1.SelectToken("result.endResult").Value<string>();
+
+            if (address1 == "OK")
+            {
+                Correct();
+            }
+            else
+            {
+                Incorrect();
+            }
+        }
+
+        private void Incorrect()
+        {
+            textBox2.Visible = false;
+            button1.Visible = false;
+            SetText("Authentication failed!");
+            label1.ForeColor = Color.Red;
+            pictureBox1.Image = codex.Properties.Resources.safeAD;
+        }
+
+        private void Correct()
+        {
+            textBox2.Visible = false;
+            button1.Visible = false;
+            label1.ForeColor = Color.Lime;
+            SetText("Authentication successful!");
+            pictureBox1.Image = codex.Properties.Resources.safe;
+        }
+
+        private async Task WaitResponseAsync(string sessionId)
+        {
+            HttpClient client = new HttpClient();
+
+            string json = "{\"channel\":\"demo\",\"sessionId\":\"" + sessionId + "\"}";
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+
+            client.DefaultRequestHeaders.TryAddWithoutValidation("authority", "smid.demo.sk.ee");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("accept-language", "en-US,en;q=0.9,lt;q=0.8,ru;q=0.7,pl;q=0.6,zh-TW;q=0.5,zh;q=0.4");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("content-type", "application/json");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("cookie", "io=KwBOqh7MODth3i0fAADs");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("dnt", "1");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("origin", "https://smid.demo.sk.ee");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("referer", "https://smid.demo.sk.ee/");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua", "\" Not A; Brand\";v=\"99\", \"Chromium\";v=\"102\", \"Google Chrome\";v=\"102\"");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua-mobile", "?0");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua-platform", "\"Windows\"");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-dest", "empty");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-mode", "cors");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-site", "same-origin");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36");
+
+            var response = await client.PostAsync("https://smid.demo.sk.ee/debug/api/getTransactionResult", httpContent);
+
+            result = await response.Content.ReadAsStringAsync();
+        }
+
+        private async Task SendRequestAsync()
+        {
+            HttpClient client = new HttpClient();
+
+
+            string json = "{\"channel\":\"demo\",\"vcChoice\":false,\"method\":\"authenticateUser\",\"country\":\"EE\",\"identityType\":\"PNO\",\"idCode\":\"" + textBox2.Text + "\",\"displayText\":\"Safe deposit box\"}";
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+
+            client.DefaultRequestHeaders.TryAddWithoutValidation("authority", "smid.demo.sk.ee");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("accept-language", "en-US,en;q=0.9,lt;q=0.8,ru;q=0.7,pl;q=0.6,zh-TW;q=0.5,zh;q=0.4");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("content-type", "application/json");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("cookie", "io=KwBOqh7MODth3i0fAADs");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("dnt", "1");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("origin", "https://smid.demo.sk.ee");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("referer", "https://smid.demo.sk.ee/");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua", "\" Not A; Brand\";v=\"99\", \"Chromium\";v=\"102\", \"Google Chrome\";v=\"102\"");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua-mobile", "?0");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-ch-ua-platform", "\"Windows\"");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-dest", "empty");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-mode", "cors");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("sec-fetch-site", "same-origin");
+            client.DefaultRequestHeaders.TryAddWithoutValidation("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36");
+
+            var response = await client.PostAsync("https://smid.demo.sk.ee/debug/api/createTransaction", httpContent);
+
+            responseString = await response.Content.ReadAsStringAsync();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
